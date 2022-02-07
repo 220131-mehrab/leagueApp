dc7859550e4090b5854165dd3e9f049268125464
Justin Freeman <justin.justin045@revature.net>
2022-02-07 17:26:54 -0600

    lesson updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Readme
+Justin Freeman's league of legends app project tracking
+
+*test update*
---
+++ league.java
@@ -0,0 +1,9 @@
+class league {
+	String greeting = "Welcome to the league app";
+
+	public static void main(String[] args) {
+		Leaguedex dex = new leaguedex();
+		System.out.println(dex.greeting);
+	}
+
+}
